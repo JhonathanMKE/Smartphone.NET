38c757428b72c0d935449bc7d5c84e85c384440b
Jhonathan Mike <jhonathan.mke@gmail.com>
2025-09-16 19:49:20 -0300

    Adding Some Tests to verify Models definition

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+//Teste 1 - Instância de Nokia e Instalação de APP
+Nokia nokia = new Nokia("1102", "ACB", "XDFS", 32);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Mapas");
+
+//Teste 2 - Instância de Iphone e Instalação de APP
+Iphone iphone = new Iphone("1103", "AFD", "XPCS", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Mapas");
